072357b40a87a78ec81ac418c0ee1ec1f02216f3
Ahmad Dizo <email-abinhamba7@gmail.com>
2026-09-03 14:56:34 +0300

    Run database migrations when application starts

--- docker/start-apache.sh
+++ docker/start-apache.sh
@@ -6,4 +6,6 @@ set -eu
 sed -ri "s/^Listen [0-9]+$/Listen ${PORT}/; s/<VirtualHost \*:[0-9]+>/<VirtualHost *:${PORT}>/" \
     /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
 
+php artisan migrate --force --no-interaction
+
 exec apache2-foreground
