4ffde6c4b4dd7ac68793d702ca614bb0039be357
Ahmad Dizo <email-abinhamba7@gmail.com>
2026-09-03 22:01:19 +0300

    Expose uploaded audio files on Render

--- docker/start-apache.sh
+++ docker/start-apache.sh
@@ -7,5 +7,6 @@ sed -ri "s/^Listen [0-9]+$/Listen ${PORT}/; s/<VirtualHost \*:[0-9]+>/<VirtualHo
     /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
 
 php artisan migrate --force --no-interaction
+php artisan storage:link --force --no-interaction
 
 exec apache2-foreground
